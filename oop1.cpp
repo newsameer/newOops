864d54d83a2445ba17a400b85d18172677113127
Sameer kadyan <newsameer170@gmail.com>
2024-12-24 12:58:10 +0530

    Class and Struct

--- oop1.cpp
+++ oop1.cpp
@@ -1,5 +1,15 @@
+// In this lecture we will discuss about class //
+   /*
+       Q1 :- What is diffrence between structure and class ?
+       Q2 :- Why we should use class as comparison of structure?
+
+       Ans :-           Class                                       Structure
+                1. Members of a class are                   1. Members of structure are
+                    private by default.                          public by default.
+   */
 #include<iostream>
 using namespace std;
+
 int main () {
-    
+
 }
